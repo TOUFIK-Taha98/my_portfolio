f2aac9284ad338abe3f9297620e2a5ca7dab7d68
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-08-22 09:17:55 +0200

    Dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,6 +32,8 @@ export default function RootLayout({
             {children}
             <Footer />
             <Toaster position="top-right" />
+
+            <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
