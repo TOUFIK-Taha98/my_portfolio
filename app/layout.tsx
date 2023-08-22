fca0004650e24461ff70550c1038e8fec45edb2f
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-08-22 09:11:44 +0200

    Contact form done - with Toast ping

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import ThemeContextProvider from "@/context/theme-context";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,8 @@ export default function RootLayout({
           <ActiveSectionContextProvider>
             <Header />
             {children}
+
+            <Toaster position="top-right" />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["plus.unsplash.com"],
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
