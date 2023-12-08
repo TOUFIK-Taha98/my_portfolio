78a335665e2dbd4378f9e9b6f296e4369207387b
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-08 20:09:11 +0100

    Header & experiences translated

--- lib/data.ts
+++ lib/data.ts
@@ -4,28 +4,28 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 
 import ticketBooking from "@/public/ticket-booking.png";
-import wobyImg from "@/public/woby.png";
+import mazaimg from "@/public/mazaimg.png";
 import placesApi from "@/public/places-api.png";
 
 export const links = [
   {
-    name: "Home",
+    name: "Accueil",
     hash: "#home",
   },
   {
-    name: "About",
+    name: "À propos",
     hash: "#about",
   },
   {
-    name: "Projects",
+    name: "Projets",
     hash: "#projects",
   },
   {
-    name: "Skills",
+    name: "Compétences",
     hash: "#skills",
   },
   {
-    name: "Experience",
+    name: "Expériences",
     hash: "#experience",
   },
   {
@@ -36,52 +36,52 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "WOBY - Fullstack Developer",
+    title: "Woby - Développeur Fullstack",
     location: "Paris, France",
     description:
-      "At Woby, I worked as an apprentice for a year, contributing to the enhancement of a collaborative tool designed to manage flex office and remote work arrangements. Throughout this experience, we followed an Agile methodology with two-week sprints. In terms of technology, I utilized AWS services such as Lambda, API Gateway, Cognito, DynamoDB, and S3. Additionally, I employed TypeScript to implement the requested functionalities. For instance, I developed reporting features using Node.js libraries to format and transform data into CSV files. This experience also involved various other functionalities that I successfully implemented, further deepening my skills in AWS and TypeScript while working in an Agile team environment.",
+      "Chez Woby, j'ai occupé le poste d'apprenti pendant un an, contribuant à l'amélioration d'un outil collaboratif conçu pour gérer les espaces de travail flexibles et le télétravail. Tout au long de cette expérience, nous avons suivi une méthodologie Agile avec des sprints de deux semaines. En termes de technologie, j'ai utilisé les services AWS tels que Lambda, API Gateway, Cognito, DynamoDB et S3. De plus, j'ai utilisé TypeScript pour mettre en œuvre les fonctionnalités demandées. Par exemple, j'ai développé des fonctionnalités de reporting en utilisant des bibliothèques Node.js pour formater et transformer les données en fichiers CSV. Cette expérience a également impliqué diverses autres fonctionnalités que j'ai réussi à mettre en œuvre, approfondissant ainsi mes compétences en AWS et TypeScript tout en travaillant dans un environnement d'équipe Agile.",
     icon: React.createElement(LuGraduationCap),
-    date: "2022 - 2023",
+    date: "12/2022 - 08/2023",
   },
   {
-    title: "Orange - Backend Developer",
+    title: "Orange - Développeur backend et Analyste",
     location: "Arcueil, France",
     description:
-      "At Orange, I worked as an intern for a year, focusing on developing multiple features that were subsequently utilized by internal teams. Throughout this experience, I employed technologies such as Node.js, Express, Google Cloud, Google Analytics, and Data Studio. My role primarily involved crafting functionalities that catered to the needs of various internal teams. This included building solutions using Node.js and Express, harnessing the power of Google Cloud services, implementing Google Analytics for tracking and analysis, and creating interactive reports via Data Studio. My time at Orange not only allowed me to contribute to the company's internal toolset but also enabled me to gain hands-on experience with a range of technologies central to modern web development.",
+      "Chez Orange, j'ai occupé un poste en alternance pendant un an, me concentrant sur le développement de plusieurs fonctionnalités ultérieurement utilisées par les équipes internes. Tout au long de cette expérience, j'ai utilisé des technologies telles que Node.js, Express, Google Cloud, Google Analytics et Data Studio. Mon rôle consistait principalement à concevoir des fonctionnalités répondant aux besoins de diverses équipes internes. Cela incluait la création de solutions avec Node.js et Express, tirant parti des services de Google Cloud, la mise en place de Google Analytics pour le suivi et l'analyse, ainsi que la création de rapports interactifs via Data Studio. Mon temps chez Orange m'a non seulement permis de contribuer à l'ensemble d'outils internes de l'entreprise, mais m'a également donné une expérience pratique avec une gamme de technologies au cœur du développement web moderne",
     icon: React.createElement(CgWorkAlt),
-    date: "2021 - 2022",
+    date: "10/2021 - 10/2022",
   },
   {
-    title: "Loria - Fullstack Developer",
+    title: "Loria - Développeur fullstack",
     location: "Nancy, France",
     description:
-      "I had an internship at Loria where I worked on enhancing a web application designed to facilitate fun and interactive foreign language teaching for educators. The application utilized technologies such as ReactJS, AI, REST APIs, and Material UI. My responsibilities included implementing new features to make the learning experience more engaging and dynamic. I collaborated with a team to integrate AI and REST APIs, while also utilizing Material UI to improve the user interface. My time at Loria allowed me to further develop my skills in web programming, working with cutting-edge technologies, and collaborating within a development team.",
+      "J'ai effectué un stage chez Loria où j'ai contribué à améliorer une application web conçue pour faciliter l'enseignement ludique et interactif des langues étrangères pour les éducateurs. L'application utilisait des technologies telles que ReactJS, l'intelligence artificielle, les API REST et Material UI. Mes responsabilités comprenaient la mise en œuvre de nouvelles fonctionnalités pour rendre l'expérience d'apprentissage plus captivante et dynamique. J'ai collaboré avec une équipe pour intégrer l'intelligence artificielle et les API REST, tout en utilisant Material UI pour améliorer l'interface utilisateur. Mon passage chez Loria m'a permis de développer davantage mes compétences en programmation web, de travailler avec des technologies de pointe et de collaborer au sein d'une équipe de développement.",
     icon: React.createElement(FaReact),
-    date: "2020",
+    date: "04/2021 - 07/2021",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "Woby",
+    title: "MazaNutribeauté",
     description:
-      "I worked as a full-stack developer on this startup project for 1 year. Users can give public feedback to companies.",
-    tags: ["AWS", "Typescript", "Jest", "Flutter", "Dart"],
-    imageUrl: wobyImg,
-    projectLink: "https://www.woby.co/",
+      "J'ai réalisé en freelance la création d'un site e-commerce avec WordPress.",
+    tags: ["Wordpress"],
+    imageUrl: mazaimg,
+    projectLink: "https://www.mazanutribeaute.fr/",
   },
   {
     title: "API Places",
     description:
-      "Is a tool that integrates Google Maps functionality with the data storage. This API allows users to efficiently manage and create location-based data.",
+      "Il s'agit d'un outil qui intègre les fonctionnalités de Google Maps avec le stockage de données. Cette API permet aux utilisateurs de gérer efficacement et créer des données basées sur la localisation.",
     tags: ["React", "Express", "MongoDB", "GoogleAPI", "JWT", "Mongoose"],
     imageUrl: placesApi,
     projectLink: "https://github.com/TOUFIK-Taha98/API-PLACES",
   },
   {
-    title: "Ticket booking App",
+    title: "Application de réservation de billets.",
     description:
-      "Flutter 3.10.0 app. This ticket booking app covers Flutter for very beginners to advanced learners. It starts from very simple ui like drawing text to complex layout using stack and mixture of column and row widget.",
+      "Application Flutter version 3.10.0. Cette application de réservation de billets couvre Flutter, s'adressant aux débutants comme aux apprenants avancés. Elle commence par des interfaces utilisateur très simples, comme le dessin de texte, pour évoluer vers des mises en page complexes utilisant le widget Stack ainsi qu'une combinaison de widgets Column et Row.",
     tags: ["Flutter", "Dart"],
     imageUrl: ticketBooking,
     projectLink: "https://github.com/TOUFIK-Taha98/booktickets",
